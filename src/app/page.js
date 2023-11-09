4bb431044fb9b0b43e9040f1caa0e569eb7deb0b
Javier Jauhari <javierampjauhari1805@gmail.com>
2023-11-09 20:20:48 +0800

    add a bio button

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,19 @@ import './globals.css'
 
 export default function Home() {
   const [Like, setLike] = useState(0)
+  const [Bio, setBio] = useState('"Today is a Gift..."')
 
   function handlerTambahLike(){
     setLike(Like + 1)
   }
+
+  function handlerUbahBio(){
+    setBio('"that is why it is called Present"')
+  }
   
   return (
     <div className='body'>
-      <div className='background-effect'>
+      <div className="background-effect">
         <div className="banner-container">
           <div className="header-banner-wrapper">
             <div className="profile-header-banner">
@@ -28,14 +33,18 @@ export default function Home() {
               <h1>JavierAMPJ</h1>
               <div className="bio-nim-header-banner">
               <p>D121211006</p>
-              <p>"Today is a Present"</p>
+              <p>{Bio}</p>
               </div>
             </div>
           </div>
           <div className="cta-banner-wrapper">
+            <div className='cta-button' onClick={handlerUbahBio}>
+              <p>Bio</p>
+            </div>
             <div className='cta-button' onClick={handlerTambahLike}>
               <p>Like! ({Like})</p>
             </div>
+            
           </div>
         </div>
       </div>
